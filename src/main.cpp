28d4980eb7476cd179605983c2d4af366b03a4ff
Hati Eth <hati.eth@gmail.com>
2015-04-19 21:24:13 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    cout << "Starting Cpp11-Random Test" << endl;
+
+#if 0
+    default_random_engine drng0{0};
+    default_random_engine drng1{random_device{}()};
+    default_random_engine drng2{0};
+#elif 0
+#else
+    ranlux24_base drng0{0};
+    ranlux24_base drng1{random_device{}()};
+    ranlux24_base drng2{0};
+#endif
+
+    cout << "drng0: " << drng0() << endl;
+    cout << "drng1: " << drng1() << endl;
+    cout << "drng2: " << drng2() << endl;
+
+    drng0.seed(0);
+    drng2.seed(1);
+
+    cout << "drng0: " << drng0() << endl;
+    cout << "drng1: " << drng1() << endl;
+    cout << "drng2: " << drng2() << endl;
+
+    cout << "drng0: " << drng0() << endl;
+    cout << "drng1: " << drng1() << endl;
+    cout << "drng2: " << drng2() << endl;
+
+    cout << "drng0: " << drng0() << endl;
+    cout << "drng1: " << drng1() << endl;
+    cout << "drng2: " << drng2() << endl;
+
+    return 0;
+}
